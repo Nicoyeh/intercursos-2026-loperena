3736d4dc09a0490dc53fc45853985dad9bddb6b0
Nicolle Pineda <nicolle15777@gmail.com>
2026-08-24 12:23:03 -0500

    fut y bask stats

--- js/data.js
+++ js/data.js
@@ -168,7 +168,7 @@ const TORNEO_DATA = {
   // js/statistics.js. Formato una vez que haya datos individuales reales:
   //
   // Si tu script consume directamente el objeto de estadísticas individuales:
-  estadisticasindividuales: {
+  estadisticasIndividuales: {
     futbol: {
       mvp: { nombre: 'Angel Camargo', equipo: '6-01' },
       goleadores: [
