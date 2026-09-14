7273ed4e449e43daa1b9e7a53f84a013ec516c57
Nicolle Pineda <nicolle15777@gmail.com>
2026-09-13 21:06:34 -0500

    no, no me puedo largar

--- js/data.js
+++ js/data.js
@@ -208,7 +208,7 @@ const TORNEO_DATA = {
     { id: 'p94', fecha: '2026-09-04', fechaTexto: '4 SEP', hora: 'Recreo', deporte: 'baloncesto', categoria: 'prejuvenil', genero: 'hombres', local: '8-02',  visitante: '8-03',  estado: 'jugado', marcadorLocal: 2, marcadorVisitante: 3 },
     { id: 'p95', fecha: '2026-09-04', fechaTexto: '4 SEP', hora: 'Recreo', deporte: 'voleibol', categoria: 'infantil', genero: 'hombres', local: '6-03',  visitante: '6-04',  estado: 'jugado', marcadorLocal: 1, marcadorVisitante: 2},
     { id: 'p96', fecha: '2026-09-04', fechaTexto: '4 SEP', hora: 'Recreo', deporte: 'voleibol', categoria: 'infantil', genero: 'hombres', local: '6-04',  visitante: '6-02',  estado: 'jugado', marcadorLocal: 1, marcadorVisitante: 2 },
-    { id: 'p97', fecha: '2026-09-04', fechaTexto: '4 SEP', hora: 'Recreo', deporte: 'voleibol', categoria: 'juvenil', genero: 'mujeres', local: '10-02',  visitante: '10-01',  estado: 'jugado', marcadorLocal: 2, marcadorVisitante: 0 },
+    { id: 'p97', fecha: '2026-09-04', fechaTexto: '4 SEP', hora: 'Recreo', deporte: 'voleibol', categoria: 'juvenil', genero: 'mujeres', local: '10-02',  visitante: '10-01',  estado: 'jugado', marcadorLocal: 0, marcadorVisitante: 2 },
  
     { id: 'p98', fecha: '2026-09-11', fechaTexto: '11 SEP', hora: '3:26 - 3:56', deporte: 'futbol', categoria: 'infantil', genero: 'mujeres', local: '7-03',  visitante: '7-04',  estado: 'jugado', marcadorLocal: 2, marcadorVisitante: 3 },
     { id: 'p99', fecha: '2026-09-11', fechaTexto: '11 SEP', hora: '3:26 - 3:56', deporte: 'baloncesto', categoria: 'juvenil', genero: 'hombres', local: '10-01',  visitante: '10-02',  estado: 'jugado', marcadorLocal: 5, marcadorVisitante: 12 }, 
@@ -420,7 +420,7 @@ const TORNEO_DATA = {
             {nombre: 'Angely Pacheco', equipo: '9-02'}, 
             {nombre: 'Thiago Gonzales', equipo: '6-01'}, 
             {nombre: 'Oriana Rosado', equipo: '7-02'}],
-            
+
       maximosAnotadores: [
         { nombre: 'Luis Daniel Linares', equipo: '7-02', canastas: 4 },
         { nombre: 'Mariana Rois', equipo: '7-02', canastas: 3 },
