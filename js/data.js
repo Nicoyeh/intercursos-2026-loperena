1d8c1a4c9ba8c2390bea0225e84f283377434e7e
Nicolle Pineda <nicolle15777@gmail.com>
2026-09-21 20:26:43 -0500

    uwu

--- js/data.js
+++ js/data.js
@@ -610,7 +610,7 @@ const TORNEO_DATA = {
       ]
     },
     voleibol: {
-      mvp: [{ nombre: 'Se actualizará cuando comience la siguiente fase: semifinales', equipo: '❤️🏐'} ],
+      mvp: [{ nombre: 'Se actualizará cuando comience la siguiente fase: semifinales', equipo: ' los queremos! ❤️🏐'} ],
 
     }
   },
